1b9234eaa2a11b9eb73897f8852a62b5b96ecbb6
monte <greg.jonca@gmail.com>
2019-01-12 14:37:48 +0100

    Add console client to chat in example-1

---
+++ src/example-1/ChatConsoleClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Bug.Net!");
+            Console.WriteLine("Let's connect to our chat!");
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://example-one-chat.azurewebsites.net/chatHub")
+                .Build();
+
+            connection.On<string, string>("ReceiveMessage", (user, message) => Console.WriteLine($"User: {user} send: {message}"));
+
+            await connection.StartAsync()
+                .ContinueWith(_ => Console.WriteLine("Connected!"));
+
+            while(true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                await connection.InvokeAsync("SendMessage", "ConsoleClient", "Greetings to all participants of Bydgoszcz .Net User Group!");
+            }
+        }
+    }
+}
