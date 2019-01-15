7e7e3205f58954227de44eaae92f4ee5f29f5bd9
monte <greg.jonca@gmail.com>
2019-01-15 20:43:05 +0100

    Scaffolded project for integration tests in example-4

---
+++ src/AdevancedChat.IntegrationTests/ChatHubTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace AdevancedChat.IntegrationTests
+{
+    public class ChatHubTests
+    {
+        [Fact]
+        public void WhenConnectingWithoutToken_ShouldFailed()
+        {
+            //Arrange
+
+            
+            //Act
+
+
+            //Assert
+
+
+        }
+    }
+}
