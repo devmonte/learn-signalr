5aa5bbc8896b81c611d08a9115bd3a28a40ec6b1
monte <greg.jonca@gmail.com>
2019-01-30 23:16:53 +0100

    Start working on streaming example

---
+++ src/example-5/StreamingExample/Hubs/StreamingHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StreamingExample.Hubs
+{
+    public class StreamingHub : Hub
+    {
+        public async Task StartStream(string streamName, ChannelReader<string> streamContent)
+        {
+            // read from and process stream items
+            while (await streamContent.WaitToReadAsync(Context.ConnectionAborted))
+            {
+                while (streamContent.TryRead(out var content))
+                {
+                    // process content
+                }
+            }
+        }
+
+    }
+}
